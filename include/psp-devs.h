013362ba0078747f7525d1c46c4e9e681e5234d0
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-01-09 17:06:29 +0100

    Stub the first unknown device area the on chip bootloader accesses

---
+++ include/psp-devs.h
@@ -0,0 +1,32 @@
+/** @file
+ * PSP Emulator - Known MMIO devices
+ */
+
+/*
+ * Copyright (C) 2020 Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __psp_devs_h
+#define __psp_devs_h
+
+#include <common/types.h>
+
+#include <psp-mmio-dev.h>
+
+extern const PSPMMIODEVREG g_MmioDevRegUnk0x03210000;
+
+#endif /* __psp_devs_h */
+
