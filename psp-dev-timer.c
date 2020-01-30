b5660b3945644df25654c98db4e18e810b7af925
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-01-30 13:22:02 +0100

    psp-dev-timer.c: There seem to be two timers one starting at 0x03010400 and the other one at 0x03010424, refelct that by implementing two subtimers for the device

--- psp-dev-timer.c
+++ psp-dev-timer.c
@@ -22,7 +22,23 @@
 
 #include <stdio.h>
 
+#include <common/cdefs.h>
+
 #include <psp-devs.h>
+
+
+/**
+ * Sub timer structure.
+ */
+typedef struct PSPDEVSUBTIMER
+{
+    /** The control register perhaps. */
+    uint32_t                        regCtrl;
+    /** The counter running at 100MHz. */
+    uint32_t                        regCnt100MHz;
+} PSPDEVSUBTIMER;
+/** Pointer to a sub timer. */
+typedef PSPDEVSUBTIMER *PPSPDEVSUBTIMER;
 
 
 /**
@@ -30,10 +46,11 @@
  */
 typedef struct PSPDEVTIMER
 {
-    /** The control register perhaps. */
-    uint32_t                        regCtrl;
-    /** The counter running at 100MHz. */
-    uint32_t                        regCnt100MHz;
+    /* Two sub timers:
+     *     - 0x03010400
+     *     - 0x03010424
+     */
+    PSPDEVSUBTIMER                  aSubTimers[2];
     /** MMIO region handle. */
     PSPIOMREGIONHANDLE              hMmio;
 } PSPDEVTIMER;
@@ -52,18 +69,23 @@ static void pspDevTimerMmioRead(PSPADDR offMmio, size_t cbRead, void *pvVal, voi
     }
 
     uint32_t *pu32Ret = (uint32_t *)pvVal;
+    PPSPDEVSUBTIMER pSubTimer = offMmio < 0x24 ? &pThis->aSubTimers[0] : &pThis->aSubTimers[1];
+
+    if (offMmio >= 0x24)
+        offMmio -= 0x24;
+
     switch (offMmio)
     {
         case 0: /* Control register */
         {
-            *pu32Ret = pThis->regCnt100MHz;
+            *pu32Ret = pSubTimer->regCnt100MHz;
             break;
         }
         case 32: /* 100MHz counter. */
         {
-            *pu32Ret = pThis->regCnt100MHz;
-            if (pThis->regCtrl & 0x1) /* could be the enable bit 0x100 is another candidate. */
-                pThis->regCnt100MHz++;
+            *pu32Ret = pSubTimer->regCnt100MHz;
+            if (pSubTimer->regCtrl & 0x1)
+                pSubTimer->regCnt100MHz++;
             break;
         }
         default:
@@ -76,23 +98,38 @@ static void pspDevTimerMmioWrite(PSPADDR offMmio, size_t cbWrite, const void *pv
 {
     PPSPDEVTIMER pThis = (PPSPDEVTIMER)pvUser;
 
-    if (cbWrite != sizeof(uint32_t))
+    if (   cbWrite != sizeof(uint32_t)
+        && cbWrite != sizeof(uint8_t))
     {
         printf("%s: offMmio=%#x cbWrite=%zu -> Unsupported access width\n", __FUNCTION__, offMmio, cbWrite);
         return;
     }
 
-    uint32_t u32Val = *(uint32_t *)pvVal;
+    uint32_t u32Val = 0;
+    if (cbWrite == sizeof(uint32_t))
+        u32Val = *(uint32_t *)pvVal;
+    else
+        u32Val = *(uint8_t *)pvVal;
+    PPSPDEVSUBTIMER pSubTimer = offMmio < 0x24 ? &pThis->aSubTimers[0] : &pThis->aSubTimers[1];
+
+    if (offMmio >= 0x24)
+        offMmio -= 0x24;
+
     switch (offMmio)
     {
         case 0: /* Control register */
         {
-            pThis->regCtrl = u32Val;
+            pSubTimer->regCtrl = u32Val;
+            break;
+        }
+        case 1: /* XXX For single byte access by the on chip bl... */
+        {
+            pSubTimer->regCtrl |= u32Val << 8;
             break;
         }
         case 32: /* 100MHz counter. */
         {
-            pThis->regCnt100MHz = u32Val;
+            pSubTimer->regCnt100MHz = u32Val;
             break;
         }
         default:
@@ -106,11 +143,16 @@ static int pspDevTimerInit(PPSPDEV pDev)
 {
     PPSPDEVTIMER pThis = (PPSPDEVTIMER)&pDev->abInstance[0];
 
-    pThis->regCtrl      = 0;
-    pThis->regCnt100MHz = 0;
+    for (uint32_t i = 0; i < ELEMENTS(pThis->aSubTimers); i++)
+    {
+        PPSPDEVSUBTIMER pSubTimer = &pThis->aSubTimers[i];
+
+        pSubTimer->regCtrl      = 0;
+        pSubTimer->regCnt100MHz = 0;
+    }
 
     /* Register MMIO ranges. */
-    int rc = PSPEmuIoMgrMmioRegister(pDev->hIoMgr, 0x03010424, 36,
+    int rc = PSPEmuIoMgrMmioRegister(pDev->hIoMgr, 0x03010400, 2*36,
                                      pspDevTimerMmioRead, pspDevTimerMmioWrite, pThis,
                                      &pThis->hMmio);
     return rc;
