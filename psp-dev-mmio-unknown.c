333e1415fb5e36d14e718d8eaf5ec66d3a79cbb4
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-02-27 15:04:17 +0100

    psp-dev-mmio-unknown: Stub register which is read to determine whether a Ryzen Pro off chip bootloader dumps its debug strings over the x86 UART

--- psp-dev-mmio-unknown.c
+++ psp-dev-mmio-unknown.c
@@ -30,10 +30,14 @@
  */
 typedef struct PSPDEVUNK
 {
+    /** Pointer to the owning device instance. */
+    PPSPDEV                     pDev;
     /** 0x03006038 register handle. */
     PSPIOMREGIONHANDLE          hMmio0x03006038;
     /** 0x03200044 register handle. */
     PSPIOMREGIONHANDLE          hMmio0x03200044;
+    /** 0x0301003c register handle. */
+    PSPIOMREGIONHANDLE          hMmio0x0301003c;
 } PSPDEVUNK;
 /** Pointer to the device instance data. */
 typedef PSPDEVUNK *PPSPDEVUNK;
@@ -52,15 +56,30 @@ static void pspDevUnkMmioRead0x03006038(PSPADDR offMmio, size_t cbRead, void *pv
     }
 }
 
+static void pspDevUnkMmioRead0x0301003c(PSPADDR offMmio, size_t cbRead, void *pvVal, void *pvUser)
+{
+    PPSPDEVUNK pThis = (PPSPDEVUNK)pvUser;
+    bool fPspDbgMode = pThis->pDev->pCfg->fPspDbgMode;
+
+    *(uint32_t *)pvVal = fPspDbgMode ? 0x1 : 0; /* Enables debug output on a Ryzen Pro off chip bootloaders. */
+}
+
 static int pspDevMmioUnkInit(PPSPDEV pDev)
 {
     PPSPDEVUNK pThis = (PPSPDEVUNK)&pDev->abInstance[0];
+
+    pThis->pDev = pDev;
 
     /* Register MMIO ranges. */
     int rc = PSPEmuIoMgrMmioRegister(pDev->hIoMgr, 0x03006038, 4,
                                      pspDevUnkMmioRead0x03006038, NULL, pThis,
                                      &pThis->hMmio0x03006038);
 
+    /* For the Ryzen off chip bootloader determining whether to print strings to x86 UART. */
+    if (!rc)
+        rc = PSPEmuIoMgrMmioRegister(pDev->hIoMgr, 0x0301003c, 4,
+                                     pspDevUnkMmioRead0x0301003c, NULL, pThis,
+                                     &pThis->hMmio0x0301003c);
 #if 0
     if (!rc)
         rc = PSPEmuIoMgrMmioRegister(pDev->hIoMgr, 0x03200044, 4,
