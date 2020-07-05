c1712c5a1267429e46f6e4031e1342f65bfd20d2
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-07-05 09:32:16 +0200

    *: Fix license headers in each file, we are GPL not LGPL, remove some trailing whitespace

--- include/psp-trace.h
+++ include/psp-trace.h
@@ -5,16 +5,16 @@
 /*
  * Copyright (C) 2020 Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
  *
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __psp_trace_h
