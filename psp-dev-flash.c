b799990804eb2afc8e463ef04559b09505b0d948
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-07-23 08:32:25 +0200

    psp-dev-flash: Remove unused headers

--- psp-dev-flash.c
+++ psp-dev-flash.c
@@ -20,17 +20,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
-#include <poll.h>
-#include <sys/ioctl.h>
-
-#include <pthread.h>
 
 #include <common/cdefs.h>
 
