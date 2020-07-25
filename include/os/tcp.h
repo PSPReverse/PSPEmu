7603545a1ac1a4a9d53f7e9ea7cca9b851ab5b4a
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-07-25 16:24:50 +0200

    os/tcp: Barebones TCP client/server connection API

---
+++ include/os/tcp.h
@@ -0,0 +1,134 @@
+/** @file
+ * PSP Emulator - OS abstraction for TCP server/client connections.
+ */
+
+/*
+ * Copyright (C) 2020 Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef INCLUDED_os_tcp_h
+#define INCLUDED_os_tcp_h
+
+#include <common/types.h>
+
+/** Opaque TCP connection handle. */
+typedef struct OSTCPCONINT *OSTCPCON;
+/** Pointer to a TCP connection handle. */
+typedef OSTCPCON *POSTCPCON;
+
+
+/** Opaque TCP server handle. */
+typedef struct OSTCPSRVINT *OSTCPSRV;
+/** Pointer to a TCP server handle. */
+typedef OSTCPSRV *POSTCPSRV;
+
+
+/** Wait until data can be read from the connection. */
+#define OSTCP_POLL_F_READ           BIT(0)
+/** Wait until data can be written to the connection. */
+#define OSTCP_POLL_F_WRITE          BIT(1)
+/** Wait until an error occurred. */
+#define OSTCP_POLL_F_ERROR          BIT(2)
+
+
+/**
+ * Tries to connect to the given remote server and returns a TCP client connection.
+ *
+ * @returns Status code.
+ * @param   phThread                Where to store the handle to the thread on success.
+ * @param   pfnMain                 The handler to call on the created thread.
+ * @param   pvUser                  Opaque user data to pass to the thread handler.
+ */
+int OSTcpClientConnect(POSTCPCON phTcpCon, const char *pszHostname, uint16_t uPort);
+
+
+/**
+ * Close the given TCP connection.
+ *
+ * @returns Status code.
+ * @param   hTcpCon                 The TCP connection to close.
+ */
+int OSTcpConnectionClose(OSTCPCON hTcpCon);
+
+
+/**
+ * Tries to read the given number of bytes from the given client connection.
+ *
+ * @returns Status code.
+ * @param   hTcpCon                 The TCP connection.
+ * @param   pvBuf                   Where to store the read data.
+ * @param   cbRead                  How much to read.
+ * @param   pcbRead                 Where to store the actual number of bytes read, optional.
+ *                                  If NULL the call will block until either the given number of bytes
+ *                                  where read or an error occured.
+ */
+int OSTcpConnectionRead(OSTCPCON hTcpCon, void *pvBuf, size_t cbRead, size_t *pcbRead);
+
+
+/**
+ * Tries to write the given number of bytes to the given client connection.
+ *
+ * @returns Status code.
+ * @param   hTcpCon                 The TCP connection.
+ * @param   pvBuf                   The data to write.
+ * @param   cbWrite                 How much to write.
+ * @param   pcbWritten              Where to store the actual number of bytes written, optional.
+ *                                  If NULL the call will block until either the given number of bytes
+ *                                  where written or an error occured.
+ */
+int OSTcpConnectionWrite(OSTCPCON hTcpCon, const void *pvBuf, size_t cbWrite, size_t *pcbWritten);
+
+
+/**
+ * Polls the given TCP connection until one of the given events occurs.
+ *
+ * @returns Status code.
+ * @param   hTcpCon                 The TCP connection.
+ * @param   fEvt                    Combination of events to wait for, see OSTCP_POLL_F_XXX.
+ * @param   pfEvtsRecv              Where to store the mask of events received upon success.
+ * @param   cMsWait                 How many milliseconds to wait, UINT32_MAX for indefinite wait.
+ */
+int OSTcpConnectionPoll(OSTCPCON hTcpCon, uint32_t fEvt, uint32_t *pfEvtsRecv, uint32_t cMsWait);
+
+
+/**
+ * Creates a TCP server instance listening on the given port.
+ *
+ * @returns Status code.
+ * @param   phTcpSrv                Where to store the handle to the TCP server instance on success.
+ * @param   uPort                   The port to listen on.
+ */
+int OSTcpServerCreate(POSTCPSRV phTcpSrv, uint16_t uPort);
+
+
+/**
+ * Destroys the given TCP server handle.
+ *
+ * @returns Status code.
+ * @param   hTcpSrv                 The TCP server handle to destroy.
+ */
+int OSTcpServerDestroy(OSTCPSRV hTcpSrv);
+
+
+/**
+ * Waits for a new connection on the given TCP server instance.
+ *
+ * @returns Status code.
+ * @param   hTcpSrv                 The TCP server instance to wait on.
+ * @param   phTcpCon                Where to store the handle to the new connection upon success.
+ * @param   cMsWait                 How many milliseconds to wait before timing out.
+ */
+int OSTcpServerConnectionWaitFor(OSTCPSRV hTcpSrv, POSTCPCON phTcpCon, uint32_t cMsWait);
+
+#endif /* !INCLUDED_os_tcp_h */
---
+++ os/posix/tcp.c
@@ -0,0 +1,305 @@
+/** @file
+ * PSP Emulator - OS abstraction for TCP server/client connections, Posix implementation.
+ */
+
+/*
+ * Copyright (C) 2020 Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <poll.h>
+
+#include <common/status.h>
+#include <common/cdefs.h>
+
+#include <os/tcp.h>
+
+
+/*********************************************************************************************************************************
+*   Structures and Typedefs                                                                                                      *
+*********************************************************************************************************************************/
+
+/**
+ * Internal TCP connection state.
+ */
+typedef struct OSTCPCONINT
+{
+    /** Socket descriptor. */
+    int                             iFdSock;
+} OSTCPCONINT;
+/** Pointer to the internal TCP connection state. */
+typedef OSTCPCONINT *POSTCPCONINT;
+/** Pointer to a const internal TCP connection state. */
+typedef const OSTCPCONINT *PCOSTCPCONINT;
+
+
+/**
+ * Internal TCP server state.
+ */
+typedef struct OSTCPSRVINT
+{
+    /** Server socket descriptor. */
+    int                             iFdSrv;
+} OSTCPSRVINT;
+/** Pointer to the internal TCP connection state. */
+typedef OSTCPSRVINT *POSTCPSRVINT;
+/** Pointer to a const internal TCP connection state. */
+typedef const OSTCPSRVINT *PCOSTCPSRVINT;
+
+
+/*********************************************************************************************************************************
+*   Internal Functions                                                                                                           *
+*********************************************************************************************************************************/
+
+int OSTcpClientConnect(POSTCPCON phTcpCon, const char *pszHostname, uint16_t uPort)
+{
+    int rc = STS_INF_SUCCESS;
+
+    struct hostent *pSrv = gethostbyname(pszHostname);
+    if (pSrv)
+    {
+        struct sockaddr_in SrvAddr;
+        memset(&SrvAddr, 0, sizeof(SrvAddr));
+        SrvAddr.sin_family = AF_INET;
+        SrvAddr.sin_port = htons(uPort);
+        memcpy(&SrvAddr.sin_addr.s_addr, pSrv->h_addr, pSrv->h_length);
+
+        POSTCPCONINT pThis = (POSTCPCONINT)calloc(1, sizeof(*pThis));
+        if (pThis)
+        {
+            pThis->iFdSock = socket(AF_INET, SOCK_STREAM, 0);
+            if (pThis->iFdSock > -1)
+            {
+                int rcPsx = connect(pThis->iFdSock, (struct sockaddr *)&SrvAddr, sizeof(SrvAddr));
+                if (!rcPsx)
+                {
+                    *phTcpCon = pThis;
+                    return STS_INF_SUCCESS;
+                }
+                else
+                    rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+
+                close(pThis->iFdSock);
+            }
+            else
+                rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+
+            free(pThis);
+        }
+        else
+            rc = STS_ERR_NO_MEMORY;
+    }
+    else
+        rc = STS_ERR_NOT_FOUND;
+
+    return rc;
+}
+
+
+int OSTcpConnectionClose(OSTCPCON hTcpCon)
+{
+    POSTCPCONINT pThis = hTcpCon;
+
+    shutdown(pThis->iFdSock, SHUT_RDWR); /* Ignore any errors here. */
+    close(pThis->iFdSock);
+    free(pThis);
+    return STS_INF_SUCCESS;
+}
+
+
+int OSTcpConnectionRead(OSTCPCON hTcpCon, void *pvBuf, size_t cbRead, size_t *pcbRead)
+{
+    int rc = STS_INF_SUCCESS;
+    POSTCPCONINT pThis = hTcpCon;
+    int fRecvFlags = pcbRead == NULL ? 0 : MSG_DONTWAIT;
+
+    ssize_t rcPsx = recv(pThis->iFdSock, pvBuf, cbRead, fRecvFlags);
+    if (rcPsx > 0)
+    {
+        if (pcbRead)
+            *pcbRead = rcPsx;
+    }
+    else if (!rcPsx) /* Client disconnected. */
+        rc = STS_ERR_NOT_FOUND; /** @todo Better status code. */
+    else if (   (errno == EAGAIN || errno == EWOULDBLOCK)
+             && pcbRead)
+        *pcbRead = 0;
+    else
+        rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+
+    return rc;
+}
+
+
+int OSTcpConnectionWrite(OSTCPCON hTcpCon, const void *pvBuf, size_t cbWrite, size_t *pcbWritten)
+{
+    int rc = STS_INF_SUCCESS;
+    POSTCPCONINT pThis = hTcpCon;
+    int fSendFlags = pcbWritten == NULL ? 0 : MSG_DONTWAIT;
+
+    ssize_t rcPsx = send(pThis->iFdSock, pvBuf, cbWrite, fSendFlags);
+    if (rcPsx >= 0)
+    {
+        if (pcbWritten)
+            *pcbWritten = rcPsx;
+    }
+    else if (   (errno == EAGAIN || errno == EWOULDBLOCK)
+             && pcbWritten)
+        *pcbWritten = 0;
+    else
+        rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+
+    return rc;
+}
+
+
+int OSTcpConnectionPoll(OSTCPCON hTcpCon, uint32_t fEvt, uint32_t *pfEvtsRecv, uint32_t cMsWait)
+{
+    POSTCPCONINT pThis = hTcpCon;
+
+    struct pollfd PollFd;
+    PollFd.fd      = pThis->iFdSock;
+    PollFd.events  = 0;
+    PollFd.revents = 0;
+
+    if (fEvt & OSTCP_POLL_F_READ)
+        PollFd.events |= POLLIN;
+    if (fEvt & OSTCP_POLL_F_WRITE)
+        PollFd.events |= POLLOUT;
+    if (fEvt & OSTCP_POLL_F_ERROR)
+        PollFd.events |= POLLHUP | POLLERR;
+
+    int rc = STS_INF_SUCCESS;
+    int rcPsx = poll(&PollFd, 1, cMsWait < UINT32_MAX ? cMsWait : -1);
+    if (rcPsx == 1)
+    {
+        if (PollFd.revents & POLLIN)
+            *pfEvtsRecv |= OSTCP_POLL_F_READ;
+        if (PollFd.revents & POLLOUT)
+            *pfEvtsRecv |= OSTCP_POLL_F_WRITE;
+        if (PollFd.revents & POLLERR)
+            *pfEvtsRecv |= OSTCP_POLL_F_ERROR;
+    }
+    else if (!rcPsx)
+        rc = STS_ERR_NOT_FOUND; /** @todo Better status code. */
+    else
+        rc = STS_ERR_INVALID_PARAMETER;
+
+    return rc;
+}
+
+
+int OSTcpServerCreate(POSTCPSRV phTcpSrv, uint16_t uPort)
+{
+    int rc = STS_INF_SUCCESS;
+    POSTCPSRVINT pThis = (POSTCPSRVINT)calloc(1, sizeof(*pThis));
+    if (pThis)
+    {
+        pThis->iFdSrv = socket(AF_INET, SOCK_STREAM, 0);
+        if (pThis->iFdSrv > -1)
+        {
+            struct sockaddr_in SockAddr;
+
+            memset(&SockAddr, 0, sizeof(SockAddr));
+            SockAddr.sin_family      = AF_INET;
+            SockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+            SockAddr.sin_port        = htons(uPort);
+            int rcPsx = bind(pThis->iFdSrv, (struct sockaddr *)&SockAddr, sizeof(SockAddr));
+            if (!rcPsx)
+            {
+                rcPsx = listen(pThis->iFdSrv, 1);
+                if (!rcPsx)
+                {
+                    *phTcpSrv = pThis;
+                    return STS_INF_SUCCESS;
+                }
+                else
+                    rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+            }
+            else
+                rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+
+            close(pThis->iFdSrv);
+        }
+        else
+            rc = STS_ERR_INVALID_PARAMETER; /** @todo Status code. */
+    }
+    else
+        rc = STS_ERR_NO_MEMORY;
+
+    return rc;
+}
+
+
+int OSTcpServerDestroy(OSTCPSRV hTcpSrv)
+{
+    POSTCPSRVINT pThis = hTcpSrv;
+
+    close(pThis->iFdSrv);
+    free(pThis);
+    return STS_INF_SUCCESS;
+}
+
+
+int OSTcpServerConnectionWaitFor(OSTCPSRV hTcpSrv, POSTCPCON phTcpCon, uint32_t cMsWait)
+{
+    int rc = STS_INF_SUCCESS;
+    POSTCPSRVINT pThis = hTcpSrv;
+
+    /* Poll on the server socket so we can time out. */
+    struct pollfd PollFd;
+
+    PollFd.fd      = pThis->iFdSrv;
+    PollFd.events  = POLLIN;
+    PollFd.revents = 0;
+    int rcPsx = poll(&PollFd, 1, cMsWait < UINT32_MAX ? cMsWait : -1);
+    if (rcPsx == 1)
+    {
+        int iFdCon = accept(pThis->iFdSrv, (struct sockaddr *)NULL, NULL);
+        if (iFdCon > -1)
+        {
+            POSTCPCONINT pTcpCon = (POSTCPCONINT)calloc(1, sizeof(*pTcpCon));
+            if (pThis)
+            {
+                pTcpCon->iFdSock = iFdCon;
+                *phTcpCon = pTcpCon;
+                return STS_INF_SUCCESS;
+            }
+            else
+                rc = STS_ERR_NO_MEMORY;
+
+            close(iFdCon);
+        }
+        else
+            rc = STS_ERR_INVALID_PARAMETER;
+    }
+    else if (!rcPsx)
+        rc = STS_ERR_NOT_FOUND; /** @todo Better status code. */
+    else
+        rc = STS_ERR_INVALID_PARAMETER; /** @todo Better status code. */
+
+    return rc;
+}
+
